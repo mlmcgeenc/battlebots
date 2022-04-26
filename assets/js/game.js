d7a67854b9a551f433023e335ea2bb5243d48bd9
Matt McGee <mlmcgeenc@gmail.com>
2022-04-25 21:20:23 -0400

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,55 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
 
-console.log(playerName)
+console.log(playerName, playerAttack, playerHealth);
 
-function fight() {
-	window.alert("The fight has begun!");
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+	window.alert(`Welcome to Robot Gladiaors`);
+
+	var promptFight = window.prompt(
+		`Would you like to FIGHT or SKIP this battle? enter 'FIGHT' or 'SKIP' to choose.`
+	);
+
+	if (promptFight.toLowerCase() === `fight`) {
+		enemyHealth = enemyHealth - playerAttack;
+		console.log(
+			`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`
+		);
+
+		if (enemyHealth <= 0) {
+			window.alert(`${enemyName} has died!`);
+		} else {
+			window.alert(`${enemyName} still has ${enemyHealth} health left.`);
+		}
+
+		playerHealth = playerHealth - enemyAttack;
+		console.log(
+			`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`
+		);
+
+		if (playerHealth <= 0) {
+			window.alert(`${playerName} has died!`);
+		} else {
+			window.alert(`${playerName} still has ${playerHealth} health left.`);
+		}
+	} else if (promptFight.toLowerCase() === `skip`) {
+		var confirmSkip = window.confirm(`Are you sure you'd like to quit?`);
+
+		if (confirmSkip) {
+			window.alert(`${playerName} has decided to skip this fight. Goodbye!`);
+			playerMoney = playerMoney - 2;
+		} else {
+      fight()
+    }
+	} else {
+		window.alert(`You need to choose a valid option. Try again!`);
+	}
+};
 
 fight();
